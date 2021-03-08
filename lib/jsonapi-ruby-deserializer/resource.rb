f7d8f7b7b7e208d04cfa5277fd17e7e28ab32776
Grzegorz Płóciniak <grzegorzsend@gmail.com>
2021-03-08 01:52:31 +0100

    Relationships rework

--- lib/jsonapi-ruby-deserializer/resource.rb
+++ lib/jsonapi-ruby-deserializer/resource.rb
@@ -14,14 +14,16 @@ module JSONAPI
           @type = data['type']
           @attributes = data['attributes']
           @links = parse_links!(data['links'])
-          parse!(@attributes)
+          parse!(@attributes) if @attributes
           parse_relationships!(data['relationships']) if data['relationships']
         end
 
         def parse_relationships!(relationships)
-          @relationships = {}
-          (relationships || {}).map do |key, h|
-            @relationships[key] = Relationship.new(h)
+          @relationships = []
+          relationships.map do |key, h|
+            @relationships << key
+            self.class.send(:attr_accessor, key)
+            instance_variable_set("@#{key}", Document.new(h))
           end
         end
 
@@ -29,13 +31,6 @@ module JSONAPI
           return if data.nil?
 
           Links.new(data)
-        end
-
-        def link_data!(index)
-          @relationships.each do |relation_name, relation_body|
-            self.class.send(:attr_accessor, relation_name)
-            instance_variable_set("@#{relation_name}", fetch_relation(relation_body.data, index))
-          end
         end
 
         def fetch_relation(data, index)
--- spec/multiple_resources_spec.rb
+++ spec/multiple_resources_spec.rb
@@ -4,21 +4,23 @@ require 'jsonapi-ruby-deserializer'
 require 'json'
 
 describe JSONAPI::Ruby::Deserializer::Document do
-  subject(:parsed_document) { JSONAPI::Ruby::Deserializer::Document.new(document) }
+  subject(:document) { JSONAPI::Ruby::Deserializer::Document.new(json) }
 
-  let(:document) { JSON.parse(File.open('spec/fixtures/multiple_resources.json').read) }
+  let(:json) { JSON.parse(File.open('spec/fixtures/multiple_resources.json').read) }
 
   context 'document contains multiple resources' do
     it 'checks every feature' do
-      expect(parsed_document.meta.license).to eq('MIT')
-      expect(parsed_document.meta.authors).to eq(['James Smith', 'Maria Hernandez'])
-      expect(parsed_document.data[0].attributes).to eq({'title' => 'JSON:API paints my bikeshed!'})
-      expect(parsed_document.data[0].title).to eq('JSON:API paints my bikeshed!')
-      expect(parsed_document.data[0].author.first_name).to eq('Dan')
-      expect(parsed_document.data[0].comments[0].body).to eq('First!')
-      expect(parsed_document.links.self).to eq('http://example.com/articles')
-      expect(parsed_document.data[0].comments[0].links.self).to eq('http://example.com/comments/5')
-      expect(parsed_document.data[0].links.self).to eq('http://example.com/articles/1')
+      expect(document.data[0].id).to eq('1')
+      expect(document.data[0].type).to eq('articles')
+      expect(document.meta.license).to eq('MIT')
+      expect(document.meta.authors).to eq(['James Smith', 'Maria Hernandez'])
+      expect(document.data[0].attributes).to eq({'title' => 'JSON:API paints my bikeshed!'})
+      expect(document.data[0].title).to eq('JSON:API paints my bikeshed!')
+      expect(document.data[0].author.data.first_name).to eq('Dan')
+      expect(document.data[0].comments.data[0].body).to eq('First!')
+      expect(document.links.self).to eq('http://example.com/articles')
+      expect(document.data[0].comments.data[0].links.self).to eq('http://example.com/comments/5')
+      expect(document.data[0].links.self).to eq('http://example.com/articles/1')
     end
   end
 end
